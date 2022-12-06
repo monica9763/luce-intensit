83a37383f12017ad394dbde7b19a163bdee12e12
monica9763 <119902292+monica9763@users.noreply.github.com>
2022-12-06 23:24:57 +0100

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,6 @@ basic.forever(function () {
             . # # # .
             # . # . #
             `)
-        music.playMelody("C5 A B G A F G E ", 120)
     } else {
         if (input.lightLevel() > 50 && input.lightLevel() < 100) {
             basic.showLeds(`
