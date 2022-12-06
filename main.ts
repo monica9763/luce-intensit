2139cf4956de7c24b642abb567f11049322d9bd3
monica9763 <119902292+monica9763@users.noreply.github.com>
2022-12-06 23:13:06 +0100

    File iniziali per il progetto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+basic.forever(function () {
+    if (input.lightLevel() > 100) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+        if (input.lightLevel() > 50 && input.lightLevel() < 100) {
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        }
+    } else {
+        basic.showIcon(IconNames.Rollerskate)
+    }
+})
